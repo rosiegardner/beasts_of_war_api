21e0d78fff25dfc3f0c254d5fc12948d749f738c
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-30 14:32:19 -0700

    test json response status in postman

--- app/controllers/beasts_controller.rb
+++ app/controllers/beasts_controller.rb
@@ -1,2 +1,12 @@
 class BeastsController < ApplicationController
+
+  def index
+    @beasts = {"quotation": "beasts api is running."}
+    json_response(@beasts)
+  end
+
+  private
+  def json_response(object, status = :ok)
+    render json: object, status: status
+  end
 end
