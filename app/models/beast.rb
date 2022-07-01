656a0730d3408df561e9ce7052b2f67cdcf938ed
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-30 23:04:26 -0700

    validate presence of columns - passes all tests

--- app/models/beast.rb
+++ app/models/beast.rb
@@ -1,2 +1,8 @@
 class Beast < ApplicationRecord
+  validates :daemon, presence: true
+  validates :breed, presence: true
+  validates :name, presence: true
+  validates :age, presence: true
+  validates :age, numericality: { only_integer: true, greater_than: 0 }
+  validates :desc, presence: true
 end
