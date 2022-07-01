1cb316799d3d6b1c517d2463607f505408660935
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-30 22:03:28 -0700

    return a update success message -passes test

--- app/controllers/beasts_controller.rb
+++ app/controllers/beasts_controller.rb
@@ -5,11 +5,27 @@ class BeastsController < ApplicationController
     json_response(@beasts)
   end
 
-  private
-  def json_response(object, status = :ok)
-    render json: object, status: status
+  def show 
+    @beast = Beast.find(params[:id])
+    # binding.pry
+    json_response(@beast)
   end
 
+  def create 
+    @beast = Beast.create!(beast_params)
+    json_response(@beast, :created)
+  end
+
+  def update 
+    @beast = Beast.find(params[:id])
+    if @beast.update(beast_params)
+      render status: 200, json: {
+        message: "This beast has been updated successfully!"
+      }
+    end
+  end
+
+  private
   def beast_params
     params.permit(:daemon, :breed, :name, :age, :desc, :available)
   end
