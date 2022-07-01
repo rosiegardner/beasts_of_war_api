b666be065fcbadaa07ae5e7d410eb05c5ad7b6fc
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-30 22:17:54 -0700

    returns success message status - passes test

--- spec/requests/put_route_spec.rb
+++ spec/requests/put_route_spec.rb
@@ -23,4 +23,9 @@ describe 'update a beast route', :type => :request do
     puts(response.body)
     expect(JSON.parse(response.body)['message']).to eq('This beast has been updated successfully!')
   end
+
+  it 'returns a success message status' do
+    patch "/beasts/#{@beast_id}" , params: { :daemon => 'shadow cat', :breed => "wicked lady", :name => "haxan", :age => 666, :desc => "a demonic cat" }
+    expect(response).to have_http_status(:success)
+  end
 end
