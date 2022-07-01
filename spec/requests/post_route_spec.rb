1778080c8263e91e3d90ea36e2542c98481d4c42
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-30 23:32:42 -0700

    DRY up tests

--- spec/requests/post_route_spec.rb
+++ spec/requests/post_route_spec.rb
@@ -6,23 +6,11 @@ describe "post a beast route", :type => :request do
     post '/beasts', params: { :daemon => 'hellhound', :breed => "red_witch", :name => "hex", :age => 666, :desc => "a good dog"}
   end
 
-  it 'returns the beasts daemon' do
+  it 'returns a beast' do
     expect(JSON.parse(response.body)['daemon']).to eq('hellhound')
-  end
-
-  it 'returns the beasts breed' do
     expect(JSON.parse(response.body)['breed']).to eq('red_witch')
-  end
-
-  it 'returns the beasts name' do
     expect(JSON.parse(response.body)['name']).to eq('hex')
-  end
-
-  it 'returns the beasts age' do
     expect(JSON.parse(response.body)['age']).to eq(666)
-  end
-
-  it 'returns the beasts description' do
     expect(JSON.parse(response.body)['desc']).to eq('a good dog')
   end
 
