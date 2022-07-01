b06b233ab48a3e63425ed0593976dcc121df813c
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-30 23:03:00 -0700

    validate presence of desc - expect pass

--- spec/models/beasts_spec.rb
+++ spec/models/beasts_spec.rb
@@ -5,4 +5,5 @@ describe Beast do
   it { should validate_presence_of :breed }
   it { should validate_presence_of :name }
   it { should validate_presence_of :age }
+  it { should validate_presence_of :desc }
 end
