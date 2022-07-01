1642c5e27348e7b93a13e125a52315138df11087
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-30 23:35:35 -0700

    DRY up tests

--- spec/requests/put_route_spec.rb
+++ spec/requests/put_route_spec.rb
@@ -12,40 +12,15 @@ describe 'update a beast route', :type => :request do
   it 'updates the beasts daemon' do
     puts(response.body)
     expect(JSON.parse(response.body)['daemon']).to eq('shadow cat')
-  end
-
-  it 'updates the beasts breed' do
-    puts(response.body)
     expect(JSON.parse(response.body)['breed']).to eq('wicked lady')
-  end
-
-  it 'updates the beasts name' do
-    puts(response.body)
     expect(JSON.parse(response.body)['name']).to eq('haxan')
-  end
-
-  it 'updates the beasts age' do
-    puts(response.body)
     expect(JSON.parse(response.body)['age']).to eq(66)
-  end
-
-  it 'updates the beasts desc' do
-    puts(response.body)
     expect(JSON.parse(response.body)['desc']).to eq('a demonic cat')
   end
-
-  it 'returns a success message' do
-    expect(response).to have_http_status(:success)
-  end
-
-  it 'returns a success message' do
-    patch "/beasts/#{@beast_id}" , params: { :daemon => 'shadow cat', :breed => "wicked lady", :name => "haxan", :age => 666, :desc => "a demonic cat" }
-    puts(response.body)
-    expect(JSON.parse(response.body)['message']).to eq('This beast has been updated successfully!')
-  end
-
+  
   it 'returns a success message status' do
     patch "/beasts/#{@beast_id}" , params: { :daemon => 'shadow cat', :breed => "wicked lady", :name => "haxan", :age => 666, :desc => "a demonic cat" }
     expect(response).to have_http_status(:success)
+    expect(JSON.parse(response.body)['message']).to eq('This beast has been updated successfully!')
   end
 end
