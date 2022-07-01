18a8dd943eb5fa38a7b4a51a1849dfd12ffb3bb5
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-30 23:02:15 -0700

    validate presence of name - expect pass

--- spec/models/beasts_spec.rb
+++ spec/models/beasts_spec.rb
@@ -3,4 +3,6 @@ require 'rails_helper'
 describe Beast do
   it { should validate_presence_of :daemon }
   it { should validate_presence_of :breed }
+  it { should validate_presence_of :name }
+ 
 end
