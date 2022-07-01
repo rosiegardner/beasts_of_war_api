68374a768ebd6f4f17c9bc5d6326c982b57c750a
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-30 20:30:41 -0700

    return a beasts daemon - passes test

---
+++ spec/requests/post_route_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "post a beast route", :type => :request do
+
+  before do
+    post '/beasts', params: { :daemon => 'hellhound', :breed => "test_breed", :name => "test_name", :age => 1, :desc => "test_desc"}
+  end
+
+  it 'returns the beasts daemon' do
+    expect(JSON.parse(response.body)['daemon']).to eq('hellhound')
+  end
+
+end
