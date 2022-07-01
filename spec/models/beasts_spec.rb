311916e35956cddf5c5f03ba375fae54bedd83b4
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-30 23:02:41 -0700

    validate presence of age - expect pass

--- spec/models/beasts_spec.rb
+++ spec/models/beasts_spec.rb
@@ -4,5 +4,5 @@ describe Beast do
   it { should validate_presence_of :daemon }
   it { should validate_presence_of :breed }
   it { should validate_presence_of :name }
- 
+  it { should validate_presence_of :age }
 end
