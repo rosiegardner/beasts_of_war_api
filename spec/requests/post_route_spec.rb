ddd7a9fba7682526af180af71ee501e38c5dac55
Rosie Gardner <rosiegardner78@gmail.com>
2022-06-30 23:16:14 -0700

    returns a created status - passes test

--- spec/requests/post_route_spec.rb
+++ spec/requests/post_route_spec.rb
@@ -26,4 +26,8 @@ describe "post a beast route", :type => :request do
     expect(JSON.parse(response.body)['desc']).to eq('a good dog')
   end
 
+  it 'returns a created status' do
+    expect(response).to have_http_status(:created)
+  end
+
 end
